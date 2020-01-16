a164141080aa7d814c3560af91b617ffe45a3643
Alex Lee <hyunho98@gmail.com>
2020-01-16 01:18:22 +0000

    functioning page scraper

--- lib/tech_deals/scraper.rb
+++ lib/tech_deals/scraper.rb
@@ -1,2 +1,20 @@
 class TechDeals::Scraper
+
+  def self.scrape_page(url="deals-1")
+    #Scrapes each item on selected page and returns the list of items as an array of objects
+    doc = Nokogiri::HTML(open("https://www.dealsource.tech/#{url}"))
+    items = []
+
+    doc.css('div.summary-item').each{|item|
+      deal = TechDeals::Deals.new
+      deal.name = item.css('div.summary-title a').text
+      deal.url = item.css('div.summary-title a').attribute('href').value
+      deal.fullPrice = item.css('div.summary-excerpt p').text.split(" ")[1]
+      deal.salePrice = item.css('div.summary-excerpt p').text.split(" ")[3]
+      hold = item.css('div.summary-excerpt p strong').text.split(" ")
+      hold.shift
+      deal.promo = hold.join(" ").gsub(/[()]/, "")
+      binding.pry
+      }
+    end
 end
